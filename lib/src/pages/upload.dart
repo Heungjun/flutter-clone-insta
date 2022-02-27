57b01ca174684ddb56cf24282f71ae4280aacf2a
Heungjun <gmdwns92@gmail.com>
2022-02-27 21:42:04 +0900

    #6

--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -1,12 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clone/src/components/image_data.dart';
+import 'package:flutter_clone/src/components/imagepath.dart';
+import 'package:get/get.dart';
 
 class Upload extends StatelessWidget {
   const Upload({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ImageData(
+              IconsPath.closeImage,
+            ),
+          ),
+        ),
+        title: const Text(
+          'New Post',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ImageData(
+                IconsPath.nextImage,
+                width: 50,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _imagePreview(),
+            _header(),
+            _ImageSelectList(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _imagePreview() {
     return Container(
-      color: Colors.red,
+      width: Get.width,
+      height: Get.height,
+      color: Colors.grey,
+      child: Container(),
+    );
+  }
+
+  Widget _header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              children: const [
+                Text(
+                  '갤러리',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.arrow_drop_down),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                decoration: BoxDecoration(
+                    color: const Color(0xff808080),
+                    borderRadius: BorderRadius.circular(30)),
+                child: Row(
+                  children: [
+                    ImageData(IconsPath.imageSelectIcon),
+                    const SizedBox(width: 7),
+                    const Text(
+                      '여러 항목 선택',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(6),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xff808080),
+                ),
+                child: ImageData(IconsPath.cameraIcon),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _ImageSelectList() {
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        mainAxisSpacing: 1,
+        crossAxisSpacing: 1,
+        childAspectRatio: 1,
+      ),
+      itemCount: 100,
+      itemBuilder: (context, index) => Container(),
     );
   }
 }
